b135b0cd7bf717e9a9827a27a44ac3c9b577c784
kasiliwachiye <wachiye25@gmail.com>
2023-11-13 11:25:19 +0300

    chore: add cors/options

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const dbConnect = require("./startup/dbConnect");
 
 const users = require("./routes/users.routes");
@@ -8,10 +10,17 @@ const app = express();
 
 dbConnect();
 
+// Configure CORS options
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+// Apply middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", auth);
 app.use("/api/users", users);
-
 
 app.listen(8080, () => {
   console.log("App is running on port 8080");
