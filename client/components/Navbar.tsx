957de270cc974e0533063a973f9f1e4e97275528
kasiliwachiye <wachiye25@gmail.com>
2023-11-14 13:12:37 +0300

    fix: rm search bar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -6,14 +6,7 @@ export default function Navbar() {
           Deluxo
         </a>
       </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-        </div>
+      <div className="flex-none gap-2">        
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <div className="indicator">
